9ed4c9501a300293a3c71883a632f945f78d2564
Snuckles123 <bernardguiller.reccion@benilde.edu.ph>
2018-11-26 16:55:16 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+  /* activate jquery isotope */
+  var $container = $('#posts').isotope({
+    itemSelector : '.item',
+    isFitWidth: true
+  });
+
+  $(window).smartresize(function(){
+    $container.isotope({
+      columnWidth: '.col-sm-3'
+    });
+  });
+  
+  $container.isotope({ filter: '*' });
+
+    // filter items on button click
+  $('#filters').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $container.isotope({ filter: filterValue });
+  });
+});
+
+// Get the modal
+var modalSignup = document.getElementById('sign-up-form');
+var modalLogin = document.getElementById('login-form');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modalSignup) {
+        modalSignup.style.display = "none";
+    } else if (event.target == modalLogin) {
+      modalLogin.style.display = "none";
+    }
+}
